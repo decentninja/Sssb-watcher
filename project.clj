7aae963f2792e54d21d9c5b5664f459c983a4bec
decentninja <very@decent.ninja>
2015-12-20 22:24:05 +0100

    Lein

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject sssb-watch "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main ^:skip-aot sssb-watch.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
