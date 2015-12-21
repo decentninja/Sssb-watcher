4324f8454852ca953e57e3c074676712c035a851
decentninja <very@decent.ninja>
2015-12-21 03:45:01 +0100

    Woohoo

--- src/sssb_watch/core.clj
+++ src/sssb_watch/core.clj
@@ -1,25 +1,28 @@
 (ns sssb-watch.core
   (:require [clojure.data.json :as json]
+            [postal.core :as postal]
             [clj-http.client :as client])
   (:gen-class))
 
 
-(def AREAS-OF-INTEREST ["Roslagstull" "Nyponet" "Forum" "Domus" "Mjölner" "Flemingsberg"])
+(def AREAS-OF-INTEREST ["Roslagstull" "Nyponet" "Forum" "Domus" "Mjölner"])
 (def SEARCH-URL "https://www.sssb.se/widgets/?omraden=&objektTyper=BOASL&hyraMax=&actionId=&paginationantal=all&callback=jQuery172047263744708705435_1450647604905&widgets[]=objektsummering%40lagenheter&widgets[]=alert&widgets[]=objektsummering%40lagenheter&widgets[]=objektfilter%40lagenheter&widgets[]=objektsortering%40lagenheter&widgets[]=objektlistabilder%40lagenheter&widgets[]=paginering%40lagenheter&widgets[]=pagineringantal%40lagenheter&widgets[]=pagineringgofirst%40lagenheter&widgets[]=pagineringgonew%40lagenheter&widgets[]=pagineringlista%40lagenheter&widgets[]=pagineringgoold%40lagenheter&widgets[]=pagineringgolast%40lagenheter&_=1450647605589")
 
 (defn download-data [from]
-  (get (client/get from) :body))
+  (get (client/get from)
+       :body))
 
 (defn parse-n-extract [raw]
   (get-in 
-    (json/read-str
-      (clojure.string/replace
-        (clojure.string/replace raw ");" "")
-        "jQuery172047263744708705435_1450647604905(" ""))
+    (json/read-str (clojure.string/replace (clojure.string/replace raw
+                                                                   ");"
+                                                                   "")
+                                           "jQuery172047263744708705435_1450647604905(" ""))
     ["data" "objektlistabilder@lagenheter" "objekt"]))
   
 (defn filter-interesting [data interesting]
-  (filter #(.contains interesting (get % "omrade"))
+  (filter #(.contains interesting
+                      (get % "omrade"))
           data))
 
 (defn show-objects [objects]
@@ -30,9 +33,47 @@
                                                    (get % "typ")])
                             objects)))
 
+(defn set-interval [callback ms]
+  (future (while true
+                 (do (Thread/sleep ms)
+                     (callback)))))
 
+(def already-sent-to-atom (atom (set [])))
+
+(defn filter-already-sent [objects]
+  (filter #(not (contains? (deref already-sent-to-atom) (get % "objektNr")))
+          objects))
+
+(defn add-to-sent [to-be-sent]
+  (swap! already-sent-to-atom
+         clojure.set/union
+         (set (map #(get % "objektNr")
+                   to-be-sent))))
+
+(defn fix-bounces [parsed]
+  (reset! already-sent-to-atom
+         (clojure.set/intersection (set (map #(get % "objektNr") parsed))
+                                   @already-sent-to-atom)))
+
+(defn send-email [to-be-sent]
+  (postal/send-message {:host "smtp.gmail.com"
+                        :user "encrypt.wave"
+                        :pass (System/getenv "MAIL_PASS")
+                        :ssl :y}
+                       {:from "encrypt.wave@gmail.com"
+                        :to "lat.zaft@gmail.com"
+                        :subject "SSSB Watch"
+                        :body (clojure.string/join "\n" (map #(get % "detaljUrl") to-be-sent))}))
+
+(defn action! []
+  (let [parsed (parse-n-extract (download-data SEARCH-URL))
+        to-be-sent (filter-already-sent (filter-interesting parsed
+                                                            AREAS-OF-INTEREST))]
+    (do (fix-bounces parsed)
+        (add-to-sent to-be-sent) 
+        (when (not (empty? to-be-sent)) (send-email to-be-sent)))))
 
 (defn -main
   [& args]
-  (show-objects (filter-interesting (parse-n-extract (download-data SEARCH-URL))
-                                    AREAS-OF-INTEREST)))
+  (do (action!)
+      (set-interval action! (* (rand-int 60) 60 1000))))
--- test/sssb_watch/core_test.clj
+++ test/sssb_watch/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [sssb-watch.core :refer :all]
             [clj-http.client :as client]
+            [postal.core :as postal]
             [clojure.data.json :as json]))
 
 
@@ -14,7 +15,7 @@
 
 (deftest getjson
   (testing "json extracted from response"
-    (is (vector? (parse-n-extract stub-response)))))
+    (is (vector? (parse-n-extract stub-response))))0)
 
 (deftest filter-areas
   (testing "filter correct areas"
@@ -22,3 +23,34 @@
            (filter-interesting [{"omrade" "Forum"}
                                 {"omrade" "Blurg"}]
                                ["Forum"])))))
+
+(deftest email
+  (testing "sending email"
+    (is (= (get (postal/send-message {:host "smtp.gmail.com"
+                                      :user "encrypt.wave"
+                                      :pass (System/getenv "MAIL_PASS")
+                                      :ssl :y}
+                                     {:from "encrypt.wave@gmail.com"
+                                      :to "lat.zaft@gmail.com"
+                                      :subject "Test"
+                                      :body "test"}) 
+                :message)
+           "messages sent"))))
+
+(deftest filter-already
+  (testing "is filtering correct"
+    (let [x [{"objektNr" 123}]]
+      (is (= x (filter-already-sent x)))
+      (reset! already-sent-to-atom #{123})
+      (is (= [] (filter-already-sent x))))))
+
+(deftest already-sent
+  (testing "if already sent works"
+    (add-to-sent [{"objektNr" 123}])
+    (is (= @already-sent-to-atom #{123}))))
+
+(deftest bounces
+  (testing "if an item disapears from data, we should remove it two so that we can again find it..."
+    (reset! already-sent-to-atom #{1 2 3})
+    (fix-bounces [{"objektNr" 2}])
+    (is (= @already-sent-to-atom #{2}))))
