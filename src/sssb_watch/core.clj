c6b4af5be23ab3bca9118c7ef174dc50a8e073d3
decentninja <very@decent.ninja>
2015-12-21 01:02:11 +0100

    Filter areas

--- src/sssb_watch/core.clj
+++ src/sssb_watch/core.clj
@@ -3,8 +3,11 @@
             [clj-http.client :as client])
   (:gen-class))
 
+(def AREAS-OF-INTEREST ["Roslagstull" "Nyponet" "Forum" "Domus" "Mjölner"])
+(def SEARCH-URL "https://www.sssb.se/widgets/?omraden=&objektTyper=BOASL&hyraMax=&actionId=&paginationantal=all&callback=jQuery172047263744708705435_1450647604905&widgets[]=objektsummering%40lagenheter&widgets[]=alert&widgets[]=objektsummering%40lagenheter&widgets[]=objektfilter%40lagenheter&widgets[]=objektsortering%40lagenheter&widgets[]=objektlistabilder%40lagenheter&widgets[]=paginering%40lagenheter&widgets[]=pagineringantal%40lagenheter&widgets[]=pagineringgofirst%40lagenheter&widgets[]=pagineringgonew%40lagenheter&widgets[]=pagineringlista%40lagenheter&widgets[]=pagineringgoold%40lagenheter&widgets[]=pagineringgolast%40lagenheter&_=1450647605589")
+
 (defn download-data []
-  (get (client/get "http://sootn.se") :body))
+  (get (client/get SEARCH-URL) :body))
 
 (defn parse-n-extract [raw]
   (get-in 
@@ -14,6 +17,20 @@
         "jQuery172047263744708705435_1450647604905(" ""))
     ["data" "objektlistabilder@lagenheter" "objekt"]))
   
+(defn filter-interesting [data interesting]
+  (filter #(.contains interesting (get % "omrade"))
+          data))
+
+(defn show-objects [objects]
+  (clojure.string/join "\n"
+                       (map #(clojure.string/join " "
+                                                  [(get % "omrade")
+                                                   (get % "adress")
+                                                   (get % "typ")])
+                            objects)))
+
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/sssb_watch/core_test.clj
+++ test/sssb_watch/core_test.clj
@@ -8,8 +8,16 @@
   (testing "HTTP package, connection and remote"
     (is (string? (download-data)))))
 
-(def stub-response (slurp "/Users/andreas/Documents/sssb-watch/response"))
+(def stub-response
+  (slurp "/Users/andreas/Documents/sssb-watch/response"))
 
 (deftest getjson
   (testing "json extracted from response"
     (is (vector? (parse-n-extract stub-response)))))
+
+(deftest filter-areas
+  (testing "filter correct areas"
+    (is (= [{"omrade" "Forum"}]
+           (filter-interesting [{"omrade" "Forum"}
+                                {"omrade" "Blurg"}]
+                               ["Forum"])))))
